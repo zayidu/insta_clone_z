5a3f7223ae9fdef5800a3f515173962d388e990f
Zayidu Ansari <zayidu11@gmail.com>
2020-06-24 17:06:55 +0400

    Insta_Clone_Z completed_24062020 - Post Build_v1

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
   MONGOURI: process.env.MONGOURI,
   JWT_SECRET: process.env.JWT_SECRET,
   SENDGRID_API: process.env.SENDGRID_API,
-  EMAIL: process.env.EMAIL,
+  EMAIL_URI: process.env.EMAIL_URI,
   MAIL_ID: process.env.MAIL_ID,
 };
